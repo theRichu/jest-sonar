74d5045beb7d6a562967f86ad7fde579103cf779
TaeYeong, KIM <therichu@gmail.com>
2022-03-15 08:16:52 +0900

    fix: preprep invalid chars

--- src/reporter/escape.js
+++ src/reporter/escape.js
@@ -1,7 +1,17 @@
 const entities = require('entities');
 const stripAnsi = require('strip-ansi');
 
+function removeInvalidChars(value) {
+    return value.replace(/[\r\b\f\0]/g, '');
+}
+
+function replaceInvalidChars(value) {
+    return value.replace(/[\t\v\n]/g, ' ');
+}
+
 module.exports = value => {
     if (typeof value !== 'string') return '';
-    return entities.encodeXML(stripAnsi(value));
+    return entities.encodeXML(
+        stripAnsi(removeInvalidChars(replaceInvalidChars(value)))
+    );
 };
